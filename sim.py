ca3ea4b6f3bfd22c840e209493ffa54f05e8b3f4
Joseph Lenox <lenox.joseph@gmail.com>
2016-02-25 21:36:08 -0600

    added debug logs

--- sim.py
+++ sim.py
@@ -7,8 +7,16 @@ try:
 except ImportError:
    import pickle
 
+class LOG(object):
+    DEBUG = 4
+    INFO = 3
+    ERROR = -1
+    WARN = 0
+
+
 def get_regular_po(gate, ckt):
-    print gate
+    if args.verbose > LOG.DEBUG:
+        print gate
     a = re.search("__TrojCkt", gate)
     if a is not None:
         for k in ckt[gate].fins:
@@ -66,35 +74,37 @@ class PySim(object):
         self.sim_cycle()
 
     def sim_cycle(self):
-        print "Getting next set of PIs"
+        if args.verbose > LOG.INFO:
+            print "Getting next set of PIs"
         if self.t not in self.current_queue:
             self.current_queue[self.t] = set()
 
         z = self.inputs.pop(0)
         assignments = itertools.izip(self.partitions, z)
-        print self.partitions
+        if args.verbose > LOG.INFO:
+            print self.partitions
         for k,v in assignments:
             for inp in zip(k,v):
-                print inp
                 self.result[inp[0]][self.t] = inp[1]
                 self.current_queue[self.t].add(inp[0])
-                print k
         output = {x:self.result[x].max(self.t) for x in self.result if self.ckt[x].function in "TP" or len(self.ckt[x].fots) == 0}
         output = {get_regular_po(x, ckt):y for x, y in output.iteritems()}
-        print "Outputs at time ", self.t, output
+        if args.verbose > LOG.INFO:
+            print "Outputs at time ", self.t, output
         self.cycle = self.cycle + 1
 
     def sim_next(self):
         if self.t in self.current_queue:
             for g in self.current_queue[self.t]:
-                print "Simming", g
-
+                if args.verbose > LOG.DEBUG:
+                    print "Simming", g
                 try:
                     next_t = self.ckt[g].delay + self.t
                 except TypeError:
                     next_t = self.t + 1
                 if next_t not in self.current_queue:
-                    print "making new queue", next_t
+                    if args.verbose > LOG.INFO:
+                        print "making new queue", next_t
                     self.current_queue[next_t] = set()
                 for fot in self.ckt[g].fots:
                     self.current_queue[next_t].add(fot)
@@ -102,7 +112,7 @@ class PySim(object):
         self.t = self.t + 1
     def run(self):
         while(self.cycle < self.total_cycles+1):
-            print "t: ", self.t
+            print "cycle: ", self.cycle
             while(self.t <= max(self.current_queue.iterkeys())):
                 print "t: ", self.t
                 self.sim_next()
@@ -117,7 +127,8 @@ class PySim(object):
         result = 0 # default value
         if gate.function.upper() in {"INPUT", "BSC"}:
             result = self.result[g].max(self.t)
-        print "Inputs:", gate.fins
+        if args.verbose > LOG.DEBUG:
+            print "Inputs:", gate.fins
         if gate.function.upper() in {"NAND", "AND"}:
             result = all([self.result[x].max(self.t) for x in gate.fins]) 
         if gate.function.upper() in {"NOR", "OR", "NOT", "BUFF"}:
@@ -126,13 +137,15 @@ class PySim(object):
             result = (([self.result[x].max(self.t) for x in gate.fins].count(1) % 2) == 1)
         if gate.function.upper() in {"NOR", "NAND", "NOT", "XNOR"}:
             result = 0 if result else 1
-        print "Gate:", gate.function.upper(), [self.result[x].max(self.t) for x in gate.fins], result
+        if args.verbose > LOG.DEBUG:
+            print "Gate:", gate.function.upper(), [self.result[x].max(self.t) for x in gate.fins], result
         return result
 
 parser = argparse.ArgumentParser(description='Simulate partitions from pickled representations.')
 
 parser.add_argument('--ff', action='store_true', help='Output fault-free response')
 parser.add_argument('--parts', type=str, default=None, help='Partition pickle')
+parser.add_argument('--verbose', type=str, default=0, help='Verbosity level, defaults to 0')
 parser.add_argument('tests', type=str, help='Test input pickle')
 parser.add_argument('pickled_file', metavar='N', type=str, nargs='+',
                    help='pickled files')
@@ -149,21 +162,22 @@ if args.parts is not None:
     f.close()
 
 for f in args.pickled_file:
-    fi = open(f, 'r')
-    packed = pickle.load(fi)
-    fi.close()
+        fi = open(f, 'r')
+        packed = pickle.load(fi)
+        fi.close()
 
-    ckt = packed[0]
-    bad_ckt = packed[1]
-    PIs = packed[2]
-    POs = packed[3]
-    t = packed[4]
+        ckt = packed[0]
+        bad_ckt = packed[1]
+        PIs = packed[2]
+        POs = packed[3]
+        t = packed[4]
 
-    to_sim = ckt if args.ff else bad_ckt
-    sim_element = PySim(to_sim, test_inputs, None) if (args.parts is None) else PySim(to_sim, test_inputs, partitions)
-    sim_element.run()
-    output = {x:sim_element.result[x] for x in sim_element.result if sim_element.ckt[x].function in "TP" or len(sim_element.ckt[x].fots) == 0}
-    output = {get_regular_po(x, ckt):y for x, y in output.iteritems()}
+        to_sim = ckt if args.ff else bad_ckt
+        sim_element = PySim(to_sim, test_inputs, None) if (args.parts is None) else PySim(to_sim, test_inputs, partitions)
+        sim_element.run()
+        output = {x:sim_element.result[x] for x in sim_element.result if sim_element.ckt[x].function in "TP" or len(sim_element.ckt[x].fots) == 0}
+        output = {get_regular_po(x, ckt):y for x, y in output.iteritems()}
 
-    z = open("results_"+f,'w')
-    pickle.dump(f, output)
+        z = open("results_"+f,'w')
+        pickle.dump(z, output)
+        z.close()
