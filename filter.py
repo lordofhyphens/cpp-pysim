f0f92f87f2e11c368ea385cbcc9184862974f1bc
Joseph Lenox <lenox.joseph@gmail.com>
2016-09-26 08:48:47 -0500

    multiprocessed filter.py

--- filter.py
+++ filter.py
@@ -2,14 +2,15 @@ import cktnet
 import argparse
 import csv
 import itertools
+from multiprocessing import Pool
 
 parser = argparse.ArgumentParser(description='Filter non-outputs and non-testpoints from simulation output.')
 parser.add_argument('--ckt', type=str, help="ckt benchmark netlist")
 parser.add_argument('infiles', nargs="+", type=str, help="input file")
 parser.add_argument('--dff', action='store_true', help='include dffs')
+parser.add_argument('--threads', type=int, default=2, help="number of threads to use")
 args=parser.parse_args()
 
-ckt, PIs, POs = cktnet.read_bench_file(args.ckt)
 def grouped(iterable, n):
     "s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."
     return itertools.izip(*[iter(iterable)]*n)
@@ -30,17 +31,33 @@ def is_dff(ckt, g):
     except:
         return False
 
-for f in args.infiles:
+def _start(f, dff):
+    print "Starting", f
     with open(f, 'rb') as results_file:
         with open('_'.join([f, "filtered"]), 'wb') as filtered_output:
             csvin = csv.reader(results_file, delimiter=',')
             csvout = csv.writer(filtered_output)
             for row in csvin:
                 cycle = int(row[0])
-                outlist = [x for x in grouped(row[1:],2) if x[0] in POs or is_test_point(ckt, x[0]) or is_output(ckt, x[0]) or (args.dff and is_dff(ckt,x[0]))]
+                outlist = [x for x in grouped(row[1:],2) if x[0] in POs or is_test_point(ckt, x[0]) or is_output(ckt, x[0]) or (dff and is_dff(ckt,x[0]))]
                 processed_list = []
                 for x in outlist:
                     processed_list.append(x[0])
                     processed_list.append(x[1])
                 processed_list.insert(0, str(cycle))
                 csvout.writerow(processed_list)
+    print "Done with", f
+def _start_dff(f):
+    _start(f, True)
+
+def _start_no_dff(f):
+    _start(f, False)
+
+if __name__ == '__main__':
+    ckt, PIs, POs = cktnet.read_bench_file(args.ckt)
+    p = Pool(args.threads)
+    if (args.dff):
+        p.map(_start_dff, args.infiles)
+    else:
+        p.map(_start_no_dff, args.infiles)
+    p.join()
