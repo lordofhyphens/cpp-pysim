082b4b5bf08815abdc82c1ec035af7f63c5860e2
Joseph Lenox <lenox.joseph@gmail.com>
2016-09-26 08:50:01 -0500

    run_random uses syntax for multiple mutant sets.

--- run_random.sh
+++ run_random.sh
@@ -1,19 +1,6 @@
-#!/bin/bash
-while [ $# > 0 ]; do
-  echo $*
-  if [ $# -le 1 ]; then
-    shift
-    continue
-  fi
-  echo "Running ${1} with ${2} inserted as Trojan"
-  rm -rf results_${1}_${2}_random
-  mkdir -p ${1}_${2}_random/part ${1}_${2}_random/bench ${1}_${2}_random/bench results_${1}_${2}_random ${1}_${2}/random
+echo "Running ${1} with ${2} inserted as Trojan"
+rm -rf results_${1}_${2}-${3}_random
+mkdir -p results_${1}_${2}-${3}_random
   
-  python sim.py --verbose 0 --ff -b --ckt ${1}_${2}_random/bench/pyTrojan_${1}.bench-ckt.bench --bist ${1}_${2}_random/input/pyTrojan_${1}.bench_inputs.pickle ${1}_${2}_random/bench/pyTrojan_${1}.bench-ckt.bench | gzip > ${1}_${2}.`hostname`.random.ff.sim.log.gz
-  
-  python sim.py --verbose 0 -b --ckt ${1}_${2}/bench/pyTrojan_${1}.bench-ckt.bench --bist ${1}_${2}/input/pyTrojan_${1}.bench_inputs.pickle ${1}_${2}_random/bench/pyTrojan_${1}.bench_*-badckt.bench | gzip > ${1}_${2}.`hostname`.random.sim.log.gz
-
-#  for i in `seq -w 0 499`; do echo "Checking ckt $i"; pypy unpack_pickled.py ${1}_${2}/pyTrojan_${1}.bench_${i}.pickle results_${1}_${2}/pyTrojan_${1}.bench_392-badckt.bench_ff  results_${1}_${2}/pyTrojan_${1}.bench_${i}*bench ; done | tee ${1}_${2}.compare_results.log 
-  shift 
-  shift
-done
+  python sim.py --verbose 6 --ff -b --ckt ${1}_${2}-${3}_random/bench/pyTrojan_${1}.bench-ckt.bench --bist ${1}_${2}-${3}_random/input/pyTrojan_${1}.bench_inputs.pickle ${1}_${2}-${3}_random/bench/pyTrojan_${1}.bench-ckt.bench 
+  python sim.py --verbose 6 -b --ckt ${1}_${2}-${3}/bench/pyTrojan_${1}.bench-ckt.bench --bist ${1}_${2}-${3}/input/pyTrojan_${1}.bench_inputs.pickle ${1}_${2}-${3}_random/bench/pyTrojan_${1}.bench_*-badckt.bench
