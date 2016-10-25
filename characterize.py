9a82ae033f7e81ca0c18d5d113d07b0b07b5936d
Joseph Lenox <lenox.joseph@gmail.com>
2016-10-24 19:28:18 -0500

    simplified and added a wealth of statistics

--- characterize.py
+++ characterize.py
@@ -12,6 +12,7 @@ except ImportError:
    import pickle
 
 parser = argparse.ArgumentParser(description='Define partitions from a file with test points and bscs in it')
+parser.add_argument('--out', type=str, default=None, help="Output file to write results to.")
 parser.add_argument('file', metavar='N', type=str, nargs='+',
                    help='pickled partition file')
 args = parser.parse_args()
@@ -19,7 +20,24 @@ args = parser.parse_args()
 for infile in args.file:
     with open(infile, 'rb') as f:
         parts = pickle.load(f)
-        print infile
         ckt = parts[0].ckt
-        print "BSCs:", len([x[0] for x in ckt.iteritems() if x[1].function.lower() == "bsc"])
-        print "Partitions:", len(parts)
+        bsc_count = len(set([x[0] for x in ckt.iteritems() if x[1].function.lower() == "bsc"]))
+        part_count = len(parts)
+        mems = [len(x.members) for x in parts]
+
+        avg_part_size = sum(mems) / len(parts)
+        min_part_size = min(mems)
+        max_part_size = max(mems)
+        median_part_size = sorted(mems)[len(mems)/2]
+
+        avg_w = sum([len(x.get_inputs()) for x in parts]) / len(parts)
+
+        stats = [ infile, bsc_count, part_count, avg_part_size, min_part_size, max_part_size, median_part_size, avg_w]
+        stats = map(lambda x:str(x), stats)
+
+        if args.out is not None:
+            with open(args.out, 'ab') as f:
+                ",".join(stats)
+        else:
+            print ",".join(stats)
+
